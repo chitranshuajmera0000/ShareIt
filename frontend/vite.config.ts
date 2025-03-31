6dde7545208bc78aec98bd90866cda7caf178f1e
chitranshuajmera0000 <ajmerachitranshu951@gmail.com>
2025-04-01 00:43:21 +0530

    vite.config.ts updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base: '/',
   build: {
-    sourcemap: false, // Disable source maps in production
+    outDir: 'dist',
   }
 });
-
