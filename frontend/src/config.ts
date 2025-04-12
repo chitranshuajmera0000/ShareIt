a267db518cd6c797f2e9eeee6a9f78059b52da1b
chitranshuajmera0000 <ajmerachitranshu951@gmail.com>
2025-04-12 23:58:16 +0530

    Frontend  Issue resolved

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const UPLOAD_PRESET = import.meta.env.VITE_UPLOAD_PRESET 
-export const CLOUD_NAME = import.meta.env.VITE_CLOUD_NAME 
+export const UPLOAD_PRESET = import.meta.env.VITE_UPLOAD_PRESET;
+export const CLOUD_NAME = import.meta.env.VITE_CLOUD_NAME;
