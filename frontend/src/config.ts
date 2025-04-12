c659bfd9e3b5f10cc1498bd62b459e0ac94de41f
chitranshuajmera0000 <ajmerachitranshu951@gmail.com>
2025-04-12 17:33:49 +0530

    Forgot to Update config file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,5 @@
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-// export const BACKEND_URL = "https://backend.1ms23ai014.workers.dev";
-export const BACKEND_URL = "http://127.0.0.1:8787";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-// export const UPLOAD_PRESET = import.meta.env.VITE_UPLOAD_PRESET 
-export const UPLOAD_PRESET = "ShareIt";
+export const UPLOAD_PRESET = import.meta.env.VITE_UPLOAD_PRESET 
 
-export const CLOUD_NAME = "dxj9gigbq";
-// export const CLOUD_NAME = import.meta.env.VITE_CLOUD_NAME 
+export const CLOUD_NAME = import.meta.env.VITE_CLOUD_NAME 
