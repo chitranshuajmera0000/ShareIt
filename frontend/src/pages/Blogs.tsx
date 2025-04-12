cd2d33492c1e948d569ab09518803869b781159a
chitranshuajmera0000 <ajmerachitranshu951@gmail.com>
2025-04-12 18:04:22 +0530

    Blogs Updated

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -297,7 +297,6 @@ export const Blogs = () => {
                                     <svg className="w-4 h-4 mr-1 text-indigo-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                                     </svg>
-                                    {post.author.details[0].name}
                                     {post.author?.details?.[0]?.name ?? "Unknown Author"}
                                 </div>
                             </div>
